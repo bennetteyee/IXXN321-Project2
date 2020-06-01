0c73b167d26ffecc691c55197c369bdc5549d09c
Bennette Yee <bennetteyee@Bennettes-MBP.lan>
2020-06-01 13:16:57 +1200

    Added styling to email form

--- src/components/js/EmailForm.js
+++ src/components/js/EmailForm.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import { FormGroup, Form} from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+   .form-group{
+       background: #EAA58B;
+       padding: 15px;
+       border-radius: 20px;
+   }
+`;
 
 export const EmailForm = () => (
+<Styles>
 <Form>
   <Form.Group controlId="formGroupEmail">
     <Form.Label>Join now for weekly updates and newsletters</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
   </Form.Group>
 </Form>
-    
+</Styles>
+
 )
